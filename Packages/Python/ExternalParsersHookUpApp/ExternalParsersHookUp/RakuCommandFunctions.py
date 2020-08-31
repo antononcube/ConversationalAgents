514bdb955aba53890aa75f788f1bcb5e886f63bc
antonocube <antononcube@gmail.com>
2020-08-31 13:02:22 -0400

    Minor code cosmetic changes.

--- Packages/Python/ExternalParsersHookUpApp/ExternalParsersHookUp/RakuCommandFunctions.py
+++ Packages/Python/ExternalParsersHookUpApp/ExternalParsersHookUp/RakuCommandFunctions.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def RakuCommand(command, moduleName, rakuLocation = "/Applications/Rakudo/bin/raku"):
+def RakuCommand(command, moduleName, rakuLocation="/Applications/Rakudo/bin/raku"):
     res = subprocess.run([rakuLocation, "-M", moduleName, "-e", command], capture_output=True)
     res.stderr = res.stderr.decode()
     res.stdout = res.stdout.decode()
