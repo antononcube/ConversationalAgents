d7f1e3ce535da3047d376d9646ed07d0d2cd191f
antonocube <antononcube@gmail.com>
2021-07-16 11:59:40 -0400

    docs:Better manifest.

--- Packages/Python/ExternalParsersHookUpApp/setup.py
+++ Packages/Python/ExternalParsersHookUpApp/setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ExternalParsersHookUp-antononcube",
-    version="0.0.1",
+    version="0.0.5",
     author="Anton Antonov",
-    author_email="author@example.com",
+    author_email="antononcube@posteo.net",
     description="External Raku parsers hook-up package",
     long_description=long_description,
     long_description_content_type="text/markdown",
