d705824a0186f6c533c92cb2fb828455ac477393
antonocube <antononcube@gmail.com>
2021-10-10 19:42:10 -0400

    docs:Using GPL-3.

--- Projects/OOPPhoneDialingDialogsAgent/Mathematica/OOPPhoneDialingDialogsAgent.m
+++ Projects/OOPPhoneDialingDialogsAgent/Mathematica/OOPPhoneDialingDialogsAgent.m
@@ -1,4 +1,25 @@
-(* Mathematica Package *)
+(*
+    Phone dialing dialogs agent in EBNF Mathematica code
+    Copyright (C) 2021  Anton Antonov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Written by Anton Antonov,
+    antononcube @@@ posteo ... net,
+    Windermere, Florida, USA.
+*)
+
 (* Created by the Wolfram Language Plugin for IntelliJ, see http://wlplugin.halirutan.de/ *)
 
 (* :Title: OOPPhoneDialingDialogsAgent *)
