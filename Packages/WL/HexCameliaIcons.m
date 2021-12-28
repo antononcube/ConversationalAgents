b773d9da89b3e6027cfb4e4808cb9169227cb0e7
antononcube <antononcube@posteo.net>
2021-12-28 10:24:12 -0500

    feat:Crisper icon.

--- Packages/WL/HexCameliaIcons.m
+++ Packages/WL/HexCameliaIcons.m
@@ -34,7 +34,7 @@ imgPerl6Hex4 = ImageRotate[ColorConvert[ImageResize[imgPerl6Hex2, {Automatic, 24
 The assignments to the private variables `rbCameliaHex16` and `rbCameliaHex24` were made by copying and pasting
 the content of the output cells of the images `imgPerl6Hex3` and `imgPerl6Hex4` respectively.
 
-## Second derivations
+## Derivation the graphics
 
 The previous steps produced blurred "prefix" icons for the Raku cells. Hence I following steps.
 
@@ -58,6 +58,27 @@ imGr2 = Graphics@Replace[imGr1,FilledCurve[x__] :> Map[FilledCurve[#] &, x], Inf
 And copied and pasted `imGr2 // FullForm` in the definition of `HexCamelia` below.
 
 Note that `HexCamelia` has parameterized colors -- that is made convenient by Step 3.
+
+## Second icon derivations
+
+1. Get the graphics of the icon and rotate it:
+
+```mathematica
+imGr5 = GetHexCameliaGraphics["RotationAngle" -> -Pi/6]
+```
+
+2. Make an image with large resolution:
+
+```mathematica
+imgPerl6Hex5 = ColorConvert[ImageResize[imGr5, {Automatic, 200}], "Grayscale"]
+ImageDimensions[imgPerl6Hex5]
+```
+
+3. Tweak the output graphics cell content by replacing the value of `ImageSizeRaw` to smaller values.
+For example, `ImageSizeRaw->{22., 19.}`.
+
+4. Copy in paste the cell content in the package file(s).
+
 *)
 
 BeginPackage["HexCameliaIcons`"];
@@ -785,45 +806,137 @@ HexCamelia[baseColor_ : GrayLevel[0.5], darkerColor_ : GrayLevel[0.3], lighterCo
 (* Icons                                                   *)
 (***********************************************************)
 
-rbCameliaHex16 = GraphicsBox[
-  TagBox[RasterBox[RawArray["UnsignedInteger8", {{254, 254, 247, 188, 186, 185, 189, 249, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255}, {253, 255, 183, 109, 119, 119, 110, 189, 255, 253, 255, 255, 255, 255, 255, 255, 255, 255}, {255, 233, 128, 129, 130, 130, 129,
-    131, 237, 255, 254, 255, 255, 255, 255, 255, 255, 255}, {255, 170, 119, 130, 128, 128, 130, 115, 173, 254, 253, 254, 253, 253, 253, 253, 255, 255}, {255, 209, 119, 129, 129, 126, 131, 135, 220, 255, 254, 255, 255, 255, 255, 255, 255, 255}, {254, 255, 170, 122,
-    127, 136, 205, 189, 207, 211, 251, 243, 192, 194, 191, 203, 255, 255}, {254, 244, 171, 126, 120, 189, 125, 120, 122, 117, 226, 180, 109, 121, 119, 116, 217, 255}, {255, 177, 115, 124, 157, 166, 60, 110, 134, 119, 155, 181, 124, 131, 131, 124, 147, 253}, {229, 124,
-    131, 119, 187, 119, 64, 89, 170, 170, 173, 213, 128, 128, 128, 131, 116, 207}, {206, 116, 132, 125, 144, 180, 61, 135, 187, 172, 212, 171, 120, 130, 128, 129, 122, 225}, {251, 144, 121, 129, 115, 176, 152, 178, 188, 196, 208, 124, 128, 127, 129, 118, 171, 255}, {
-    255, 214, 132, 140, 131, 205, 255, 207, 195, 204, 149, 123, 133, 131, 129, 135, 239, 254}, {255, 255, 248, 247, 246, 255, 228, 123, 123, 122, 126, 123, 156, 239, 236, 243, 255, 255}, {255, 255, 255, 255, 255, 254, 238, 134, 129, 132, 133, 119, 185, 255, 255, 255,
-    254, 255}, {255, 255, 254, 254, 255, 253, 255, 198, 110, 120, 113, 137, 248, 254, 253, 254, 255, 255}, {255, 255, 255, 255, 255, 255, 255, 252, 194, 187, 183, 223, 255, 254, 255, 255, 255, 255}}], {{0, 16.}, {18., 0}}, {0, 255},
-    ColorFunction -> GrayLevel,
-    ImageResolution -> 72.],
-    BoxForm`ImageTag["Byte", ColorSpace -> "Grayscale", Interleaving -> None],
-    Selectable -> False],
-  DefaultBaseStyle -> "ImageGraphics",
-  ImageSizeRaw -> {18., 16.},
-  PlotRange -> {{0, 18.}, {0, 16.}}
-];
-
 rbCameliaHex24 = GraphicsBox[
   TagBox[RasterBox[CompressedData["
-1:eJxlUs1PE0EUbzx59Gjiyf/CEwkHo4STNXryArE0Xrak7e7sV7czb5f4Qe1F
-D3LAr0CNVRsbtGiIVMUQhRgNxUI0QqIBaf0CjdBadp+zXWLc9Ze8yZu8+b2Z
-9/vN/t5YuG9XKBRK7OZLuId0xuM9ytE9fHNMSJyKCpGTXUIyEo3ED/S6x/by
-2McD0cH660pl/tVXngXg/M72S6IUu2gHSzY+JRYAWGSG5z4OblhpysGMs7/8
-LR2nIINbokBKAdoP6oEBtTZ9NHuHxVRZKG633DZteHeZBqNMGx67h3bTT8MX
-MjA9+3mqPHF/Zr356croyOjVcfeM03p7xjDFhUyfLhFxyRlKqIqiioto2/gk
-CYyMzHWeOM8GtMt1jc8Ipn6h5eC6mWY0tSQe7EqaqhGfLRJoTzKNWCDA5Juz
-HUe6w7ncjWsTKxprCzCwsaryLG2+ix4+RCZLD4urJU8BIB+/AZcJxDvTHcfv
-lsYKUx90Txo5h/hYMrkS8oKkjOdv579c35GN1tFpZpV0iiae1Z4vVudWKsTt
-QkF64D5+3hocPH2pgY1H5XI5k6LAi6nMZlurxlZjaxvtfIxIksGMc8u12tpP
-n9cZwwTgaupr/1rGyTa+9GaVb3E3nICnQxr/CBS+//d9bFwWRYkIkwGrPbyv
-VN9UW385fwDg3Pmk
-    "], {{0, 24.}, {27., 0}}, {0, 255},
-    ColorFunction -> GrayLevel,
-    ImageResolution -> 72.],
+1:eJztXelzE1mS79hP+3H+hf0nNvbDxkQ092WL7pnZmJmNmJnuGKa3Y3d7NxjA
+KpVU0ivJtnyCD6AxDTTNDca3zdHQPrDNDcaAwc1lfGEbfFuHJVW+rapXFpIs
+Wa8OcDm2E1NIBVS9X2W+zHz5MrP+6cv/+fxv//DJJ5/8/R/Fw+dfZH26ffsX
+zG9+JX753Td///qrb7b9deM3O7Z9tW37v3wp/bMb4u9+8Tf+hf6/EKij5R7u
+L5SERgemxsYHX4wPjw8OTA4PDfe9GX09/Gp6ZGL4+cjI2NDr8ZGBN4PTo32D
+LyfeTAwP+pZ7vCpJEKo4d7bbw7s8vIeXfnjkdiMeyefcbjePPG7ezWeL56R/
+4mFPrCxBFfAT1uVyiiQfnMonl/P9OVf0NPlqf4hXEETAoX125FJDPFseWUFc
+FHAHow6gy4XY6+L/WyEEMJ3HqUbIZY+tGDkFXMfwKgGKcso0rRSEgAdUw5OJ
+e71CIIJw1KaehaKcMicDK0LZAO5m1U7CBYgdK0HZAPhLVVqKKEJHaWAFyCng
+FqsWGZUh2i+aHyHg8RynRoCSFzRieoiAz2uwFAvE274Pm3wmAn7JacbnkpTN
+C5MzESLfaVSkCkL7Pr+pLQbgO5pM4XvibffMzEQAX7FDDwsli1E0ZWImAr6s
+Q80oTLTWmpeJAG88Tn0sFJno9LwxLUTAp3WzUFpjHDMrQsC9OiehQs6X5oQI
+EPxWl6VYINFi+EypbABfVx26SAHR2mFGJgLMFBgjpC7EFU+YkImAGw1QM4T4
+nRfNt1AEPGQMAwm5n5hOTgH/YDMOImIOmS16KuBHGhf2KSA6HplMTmG+zFCE
+LkfhtKnkFHCrYWqGEM9cNhNCgHGv6iB3OuJGTQQRcLXm6FMqQrbz5tE1gPu0
+B59SE/vcLEwEiBwx0FIsEGKPhs2CEHd9AIAkBG4KiAD+PQY5pAkIHcXmWGMI
++IrBliIK0XbJDEwEPOb5EHpGIqd7crnhYQnhmQ/EQim0WLn8TJRCFx8In0Si
+e7rcEAHXGLSyT0aixTABwkadUe6lEVaYAOGY/hhpaoSOp8uOUDSHNw2JsCUj
+njllAoMIWFCb/0RNTrcp1hcCfv6B7CFvNckaEcDIEM17Qo4ic3ht4mN+8UGY
+yNvumCVWI+BLH8CtQex3EVNwEEtMnC7UtXufnLgXZmGhxMS7WW6D8fHMeXOo
+GUIQKDdY2SBnzltTIcQvDZ6IPNNsHhmVyOg1FLKXBmIvD8vOT8AjuUYqG8Q+
+iMcUXi5kUQLcZCATedv3AsRc+9WRorvLLrTgq9CaVqpQ9IP0uT+WhXBsl3X/
+/HILqoAfaHLAEecgZOccdvGXw263O3bUxQHEdTvtu2eXGyEGYb8GJiKHt7i4
+tKCwpKg8r7B8z97dBWX7yg+em4lHOHK48MayAxQH8IxT7Z+Ki/i3QgjCIYhE
+5sIg4GAoOZBlhycR4LOsemXTn/RSkHhpk1DAqzL4zTPGJbEBKXnEivWMHhLO
+ATlovUenupmIuLxJTTeDRWz9WFwOl6uCyDPXlKGBkLyyNOF09EbBSOKtQxPz
+ganZyblA0D85HpzzT/mCM76JqcDUzPRU0O+fHvPPBiZngzNzUxO+2alpjeWO
+gB+oEVNkL5+XBy2o4wDMX8w/OBevbR8VevZ43R53gTcvF2Xneby8J9+d4+Zz
+PG63Nz/X63bni+fEg8fDZ+e4XUc07twBnFMRPUX2HtlREe/Vcbr27MWmynNn
+zl+sbTh7vKGq4fT5pnNV54/XVJ86d/pidVPl0bqaytvyTQTcY0VZLTEIAWYL
+GZeDczqdHCcfxV/Sj0LK15hzLmubNsEW8CA9D5HtuMw80Wv/+dR21paWGKe8
+JhbwK86282osQnzJyrvUzA/EZc9qUzeAL9C7p2hYGqXIkn/NCxdxbpSO3LbD
+Cs/b9zXFOHGA37hV4XOROJ4mRxfwlIcyK4O3NpHxCsz6zfduU0m3M6lsAT6l
+2utHXG6fVjltp7sbchROS3ISwVfWfrbWKpRRaGHkyJslmCBOzfRq8Ih55qhK
+Bfce4yGqgAay3ZTNmjC9bb3Fsu5qL80WFs9UL3rwACEtDrH4uO5rldOHNGLK
+swdkD1TAx1dtzbCs/zp8nubBOLMXpbkLuFPTypRn94e0KptTNGN1PCNqZuB3
+mzMzMixrzk7Q+O1SYRQk3G46X1t0AbGt2maiuCJ3pR0rz5whMorzV1syMjIy
+t2QMXqGRU8R0xcuWgOu1Bhc4t8ra6gWPVsAX0jKR87wlpu3+RpGDIm39tHw+
+l8IhQvb9QYi76QBSaykWiLfWq5qJSjhMOswXphkrb71CLEV4+zqLjDAjc/2d
+m1aaCWy9FjssfdtCTnUW4/6Ruqq6ymNnqiqrspfeFkaO3X4yC5vWKgAzLOv+
+O3KIxihy3hhlI+CHOvZnkf0w/e4W4FGX1WrNYmyMlbGmmYfIdk/iA8DUnzdl
+ZixAXPvjz1RMzDodHRVAQFfmLr/zOjUTJQ3q5qUWJtIhHcDDETJhD65eYKEo
+phv+4j9Hk8SJXEMLwxJwq660T1GYQpQABSmthvphOl8RGX3+2ZYoCyWLcXqU
+pRlWNGkR8HiuvsxdZGuhZCKE6SuCeGuV4kDzMSwUaYvlRQuNnCJGydAAqNKb
+zOPMocsnF/B1+nUhlztBWHh9Q2YswIytq7wCTd0N4oomSCBjQhc6+Vq2MzT7
+sKLKKKCWFp5pJVGjwH+tj2OhOBU3371Lx8QfyYOfL9Of92mnSdkB3EQ94ZG9
+LEjUTNXqrfEARXW6Ex+mMW9cwSQRg/tZumtWbRSJZaoqghAr14oIMPbHTZkJ
+CEWzf2WCZsiIrDEA5vWVjsvXst9PCxHgGLVfgWw/KK5d+WpLIsCMzE2/Haun
+YAtyZveRmfhcd7YScnjfpTH7gB/TP0gnGiQO6dNF8Ih7WjiXR7FUQPZS5eE2
+6N5e55mapZkIaiqCeGuDfDUhwq5ZzEKRiRmZA+07acy+/Tph4qT6hk2J5PT0
+LwlRwG3UyxfE5U8TFdGyLhlAUdms8gbLKR4YchT6CET9uTyIOb5opySOJugr
+gnimkzikvq/WL1IzCsTVLQNZNBaDvUyUzcweDftBCddy9S7BRMDV9Cxk94cJ
+208ushRROd341STVgsgpLn0EOQ6lO+dMtGCp1xhyRRC1IiUJsQIe+rfNKVgo
+KptfV0/QjJm3nsJKQFl30RWflbp/Ewj0FUE8c1KxFEUpWSg5Nn94W0u1xnA8
+JjOxP81ylIK4wokUcip6FSq0tXuE/J/uzSnxycome7qIxj21VciltKA9UvP+
+WtbG5EwUVcYeekvBXCJrisjOtckVqcLEzI0v7+yishg3iLIZz9ftniLRPU0G
+EfBVeofUUTwH8iy8tCRAyT3dMbuX5sFxXmWNcVu378bbvkumawT8NptakSHm
+jqxmYPqLjUsjzMhYf/vFjtTJjmjhwCuFURDZq9uzQfYnSZgI+By9pbCRhFjA
+h5M4pAlyuuGvk0d3IXnLTzQLZEtQsg/y9p/4deGjk+snTOwxwGKUzi3iouj3
+Uss/cqHnxHz1/WZLGoCinH763XQJj7x8Pu9y8l4vEj+7XDm8NwflevIK3S4+
+txC5UZ7Hel2xGMd017Uk60wJ4QpqdwKxB4lDivNWpbYU72fi/2II+kLB8JR/
+1h8OzAV8Yd9s2B8K+0Lz85H5OV84NDvvCwd8EeVZG2D2ne7EPRHAt1TshnJ5
+ikd6Io2ekRGuduKXjx886OntefL4wdOe7p773U8fPOy6/6i7q+vBk0c9XY+l
+c11Pu0aV53ZVf1kLYs8m7rfN0litBeKtlSBL6cyfNqT0Z6ITcWP3cGWVSNXR
+Q1KqrnxD5uGo24BqecTGN4sT1GxoS0S0gmgs0iK0rC7El2sbG9JRY829BcfN
+iNRPnv0hXkaHeVWyj9hyae8dhAiXTk43/3bk58qG+rRU1zRLHtrPBrVvct2O
+RQgnVcYqEdtOxnN389IIt646FapLj6++oYqEySBkTPsm0WIUhmJk9Ild7QU4
+7xzZud/z66XUqWXD3/x3qtKzsKH2aogM5YZhRZ6eqehMBNUduyWLc5WE2V79
+eXGYLQbh2rbp8xQy2lAzqKyB6YO16QbYEAVInXIRS8hRMAwJm2pJAK5hobmG
+goXVnSTr0LAMc8QVRFkIeDpPQ4Ezb/uBPPW5/0gMd7+nLVt6B2lYWF8/TiyF
+Ye2blBiLglBLx27Jb+gmscQfN6YSU8uqMqGJQs80VJN6BRCOGcXCmOQMkYVu
+TV4SbztA+j/BjhSxtsxNvx9/TMHChrqLPjKUR4YV6cYVHUeOa/N0EdtJLEbv
+H5Irm62rqoO1FBLaUP2MDIcq9kg1NNupGIACfqWtzwDiPG+JsjnwaTKLYVn/
+n6Eb1RSzsLYlQgZyzahCloSiY8AN2oRDyteTlc3UtmTOm2XdrXEKUygiVBxS
+FcHaNANj4tuMAPbla7w0UtzT80nC+pY1PFytpbEUN5U1RY1RLHQUzSUunVq1
+MRGxx0Nyuvz0YiZmZlj6+iobKVjYMEVk1LD2TWhx0TEIJVrl9BaxGN3rEhFu
+/fSQ0FhPw0LSEASAaj+VBiB7cFEan+beSchRMi8zIJKT4NlYNv5pqpvKIb0c
+JCy8b5SlQFySPuIApzXKqU3ZGBvaGh8Ztqy+4KuhEdHqV0Rb+emDtUtT8qJj
+wINI2yzgsmcJxP1xUTfLuu2RdhqHtKZNidpRZTNSEOJy3iaLlooWQ5uc8raq
+CFE2X22IyYnK2Ng1dp6ChfW1o+QBvc02qGyVtzYn3bUQjZG2jCTksivpsxfW
+ZMbIaB5cpnBnGqrvKGrmrFEOqb0kRat7QWs/SGTfJ6dAQ2BX1D3N3Pz58DOa
+NUVd0wxh4TOjeqhJRcfJ99YAwge1TXUlOTGCb0fV6dZVx8P1VJbiCVEzIa1V
+uYsH8/0S26P9Lk0JuuJCRa54EsKHlJ3EzI1f+u/R+Gu1V5QMeBXB2qXJ6RpI
+nSoMwklt+6/I3qQEOv+drDEsa5pnqtPjExWpslMxV2hQjzFkrUuqZqJM1Lb/
+ipyeUaJsvpOZaFlrFdqoLEW7Erq4QJMAR0NO79QSCCVloy2zjLedJCWdM9vk
+rIxNT4bpQhfvCAvfqAvWLjEQpn3JZBoAn9Y0ZPSSjLVxs6RmSvEFmjVF1X0l
+yH3CMId0b3DpnC8Bd2m0GEqBjwDuVZ9t+v3oEypLccFHbtpjmKWw96TL24P5
+cm3vIGO7FHY8/dyyupIydPGzkpK4V3eWECGeOQHpSi4AP9FUdWQ7KhDpEHDF
+P38dulWddh+mobG2OUSSLzqMa3A/nI6F0v00veEJLWTLAfTv7B2rrBGptram
+prqmlnyqFo/koFB1TdUQYeGk1vjCInzMhfQApWQPmnzJhEtb6+Mu/by5s6Wl
+vaX5WktnW0tHW0dLc3tnR0dz+/WO1ub21o72lmvX2m9c61EeaZ1BnaeRo5Cq
+VhbwRbUWAznyYwrxpTr0iBAKRaQfiISEsPRFEIRQBITwvPhV/BSJKI6H7EgZ
+Q7ztFl3hEwTVJpSnM0Ipn6X0OCJHDbMUFZT90lTvtC2sLbSQoPn1iouJe0Zb
+uwaRI6ruihzJsnMo7xUsMcgW8sxZFWVdr9X4UIg5kUJGQUhHYdxs2JrCo6LA
+EqBRDRN5He+seqfj9Yrxg2CuqBKk+UL6VGhrijdVAn73U9tP7bdaL7ddvtl2
++cqla63trT92tl9tab597ceWizfaOluv7jdqI8a+x6/mOQu4nSZfUr50SiME
+kYodTJaNtWZZs8RDVpbNylizbEyWlWEZ+ZzNusuobrCITV9IEv/4faUcn7bv
+g9z7gbmdXDoEfJNx8+JFomWMiw7iHwYB5JUaSDUQu3c6WDvLSr/tDvkDOUh/
+2OWz8lfHrorkZfDiUjFd+bCBhFSWAEsDDLcUl5WUVRwoLS0vLyrdX3Zgd8m3
++yr2lpaWHjhUUl56oKz029KK0pI9Za9SzUL68rD0AJJ8jT3H71rcnIGCghAW
+V+0RjEM+qbA7IMliWPTHpJ6AcmRN6g2UytQDHjJi2c45HVKLGgfn5JCDczjE
+aS9+l7665INT/Cz+OA5peVEPRA/RLwCxfwHx/yThf4P+FFGXM7tv7NXgyNBw
+78DbN+9eDL4YmhzuHx7q6x0YHXrb2/9u9F1/f9/g2IvXI3r7MGp5PCrKw1IS
+cnR/uCHqJDAk4wA58sZTNdVK0VProwE0KOOAt50zR4PsRAI8adgLo/qWvQ1o
+MhJwjVHLdvaIaXpkx5DKtcnSEBnzvVhQXXlYWoT2soDppqKBGQcSRCtN/Oyj
+EuDQfoMCvDJCrtB0qgaEegPf5MIzjaaTUnHlq7NFSQxJy0+zSam8rDBO0zA3
+zWgQIXDAIDlFSh84s5HUKtuoQH2vGVkoyelxQ5wapQ+cCQnwsBHKBnEeU7xd
+KBkJ+CcDFhcmewNtHAHM6l9dIMdujR2QPwYB7tBf6EpSrcxKEC7V6Z3y7CGt
+TVc/CgF+7tbZ/YFLEag0CwFW0yp7MaEkDWrNRYBncnQwEXG546ZzuRNIQ0JA
+DPFWjS1lPyIBvCvQzERTru0XEeBHmtcYiH1oZkuxQBA6qtFiULXOMwEBfkXf
+kymOgy7XwIpAKE7FSm3NuZn6lQFQCn7TJwTEkNIHbiWQgFtoEwLiWEjfD3jZ
+CfzqN6GQXWsD+eUgAXep9t0QVZdV05CAK1VClF+MvIIQAh5RuaHv9OjIsVoO
+AlylymLw1ksrC6BU503zDoEFQo5ijS/FWT4C3KIGoe32CmOhxESB6h0CREbZ
+g2bc9E1DgJ/R89BML0amJ8Cn5B1FJLeyWzgQ1O+/Sid4W+WKk1GJBHjNkmwt
+6SBlakm5WlLaFqdkbSnnnHbPuxWJEEPkel5ebn5Rtrcgryg7tyi/xJtbvDsv
+2+vNK87NLcrdnZNTVFBc4M11d65MgBL5/IFgxOcPBYMTgfB8eCYQioRmfPP+
+cMAXDoQn/eFQ2Of3BdJfyKy0YjmjhSD+gOM//UK/UBL6P3XAtuU=
+    "], {{0, 200.}, {226., 0}}, {0, 255},
+    ColorFunction->GrayLevel,
+    ImageResolution->72.],
     BoxForm`ImageTag["Byte", ColorSpace -> "Grayscale", Interleaving -> None],
-    Selectable -> False],
-  DefaultBaseStyle -> "ImageGraphics",
-  ImageSizeRaw -> {27., 24.},
-  PlotRange -> {{0, 27.}, {0, 24.}}
+    Selectable->False],
+  DefaultBaseStyle->"ImageGraphics",
+  ImageSizeRaw->{27., 24.},
+  PlotRange->{{0, 226.}, {0, 200.}}
 ];
+
+
+rbCameliaHex16 = ReplaceAll[rbCameliaHex24, HoldPattern[ImageSizeRaw->_] -> ImageSizeRaw->{18., 16.}];
 
 
 End[]; (* `Private` *)
